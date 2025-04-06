e67f74a2def761bb03872f49f25d80fa7364b0a4
99Subah99 <tafannumsubah03ctg@gmail.com>
2025-04-06 11:36:38 +0600

    acp.html

---
+++ After class project css/dom2.js
@@ -0,0 +1,20 @@
+< !DOCTYPE html >
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+
+                    <h1 id="heading">Welcome to the DOM</h1>
+                    <p class="description">Lorem ipsum dolor sit consectetur adipisicing elit. </p>
+                    <ul id="list">
+                        <li>Tafannum</li>
+                        <li>Meherin </li>
+                        <li>Tanha</li>
+                    </ul>
+                    <button onclick="changeText()">click</button>
+                    <script src="dom2.html"></script>
+                </body>
+            </html>
